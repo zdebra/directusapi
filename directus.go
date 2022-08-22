1e89aad01f0c077913b9cc1798b82a3995d95f07
zdebra <zd.brabec@gmail.com>
2022-08-22 11:58:06 +0200

    structfield

--- directus.go
+++ directus.go
@@ -224,40 +224,49 @@ func (d API[R, W, PK]) jsonFieldsR() []string {
 	return fieldsR
 }
 
+// iterateFields returns fields for all struct's fields
 func iterateFields(t reflect.Type, prefix string) []string {
 	fields := []string{}
 	for i := 0; i < t.NumField(); i++ {
 		f := t.Field(i)
-		tagVal := ""
-		if v, ok := f.Tag.Lookup(tagName); ok {
-			tagVal = v
+		fields = append(fields, structFields(f, prefix)...)
+	}
+	return fields
+}
+
+// structFields returns fields for a signle struct field
+func structFields(f reflect.StructField, prefix string) []string {
+	fields := []string{}
+	tagVal := ""
+	if v, ok := f.Tag.Lookup(tagName); ok {
+		tagVal = v
+	} else {
+		tagVal = f.Name
+	}
+	switch f.Type.Kind() {
+	case reflect.Struct:
+		p := prefix
+		if p != "" {
+			p = p + "." + tagVal
 		} else {
-			tagVal = f.Name
+			p = tagVal
 		}
-		switch f.Type.Kind() {
-		case reflect.Struct:
-			p := prefix
-			if p != "" {
-				p = p + "." + tagVal
-			} else {
-				p = tagVal
-			}
-			fields = append(fields, iterateFields(f.Type, p)...)
-		// case reflect.Pointer:
-		// 	panic("pointer not impl")
-		case
-			reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.String,
-			reflect.Pointer, reflect.Slice, reflect.Map: // todo: change this
-			// field is not nested
-			v := tagVal
-			if prefix != "" {
-				v = prefix + "." + tagVal
-			}
-			fields = append(fields, v)
-		default:
-			panic(f.Type.Kind().String() + " not implemented")
+		fields = append(fields, iterateFields(f.Type, p)...)
+	// case reflect.Pointer:
+	// 	element := f.Type.Elem()
+
+	case
+		reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.String,
+		reflect.Slice, reflect.Map, reflect.Pointer:
+		// field is not nested
+		v := tagVal
+		if prefix != "" {
+			v = prefix + "." + tagVal
 		}
+		fields = append(fields, v)
+	default:
+		panic(f.Type.Kind().String() + " not implemented")
 	}
 	return fields
 }
